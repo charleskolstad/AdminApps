1f3b63dc9d03d18e80a19f1b40fefc0aeefdc00c
Charles Kolstad <charlespkolstad@gmail.com>
2018-06-27 06:28:51 -0600

    push

---
+++ AdminApps/AdminApps_Web/Assets/JS/angular/AdminModule.js
@@ -0,0 +1,7 @@
+﻿angular.module("Admin", ["ngRoute"]).config(function ($routeProvider) {
+    $routeProvider.when("/allUsers", {
+        templateUrl: "/Admin/AllUsers"
+    }).otherwise({
+
+    });
+}).constant("dataUrl", "http://localhost:52055/");
